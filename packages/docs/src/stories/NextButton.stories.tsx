e84043015e644623377de06a60a53a6b3bd1155c
Pedro Duarte <pedrohlduarte@gmail.com>
2023-07-04 22:36:14 -0300

    feat: add next button

--- packages/docs/src/stories/NextButton.stories.tsx
+++ packages/docs/src/stories/NextButton.stories.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@nextime-ui/react';
+import { NextButton } from '@nextime-ui/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'Example/NextButton',
-  component: Button,
+  component: NextButton,
   tags: ['autodocs'],
   argTypes: {
     backgroundColor: { control: 'color' },
