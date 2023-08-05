6cdf82698e5f9b5a4bfccf14c5db433fd8e8c658
Pedro Duarte <pedrohlduarte@gmail.com>
2023-08-04 21:54:02 -0300

    feat: add aspectRatio and container

---
+++ packages/react/src/components/Container/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react'
+
+import {
+  Container as ChakraContainer,
+  ContainerProps as ChakraContainerProps,
+} from '@chakra-ui/react'
+
+export interface ContainerProps extends ChakraContainerProps {
+  children: ReactNode
+}
+
+export function Container({ children, ...props }: ContainerProps) {
+  return <ChakraContainer {...props}>{children}</ChakraContainer>
+}
+
+Container.displayName = 'Container'
