c42c93ed984a3098854375559035f33925939a50
Pedro Duarte <pedrohlduarte@gmail.com>
2023-09-10 21:00:00 -0300

    feat: add icons

--- packages/templates/src/components/templates/NextLayout/index.tsx
+++ packages/templates/src/components/templates/NextLayout/index.tsx
@@ -1,29 +1,7 @@
 import React, { PropsWithChildren } from 'react'
 
-export interface NavItem {
-  children?: Array<NavItem>
-  href: string
-  label: string
-  subLabel?: string
-}
-
 interface NextLayoutProps {
-  navItems?: Array<NavItem>
   title?: string
-  pathname?: string
-  description?: string
-  socialNetwork?: string
-  logoSrc?: string
-  logoWidth?: number
-  logoHeight?: number
-  logoAlt?: string
-  logoSubtitle?: string
-  logoSubtitleColor?: string
-  keywords?: Array<string>
-  bg: string
-  siteName?: string
-  socialImageUrl?: string
-  isRouterProtect?: boolean
 }
 
 export function NextLayout({ children }: PropsWithChildren<NextLayoutProps>) {
