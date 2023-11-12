0c1e471dee380049cfbcc0d4f9446b6bc48b51b7
Pedro Duarte <pedrohlduarte@gmail.com>
2023-11-12 15:37:24 -0300

    feat: add project rohan

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,114 +1,283 @@
 'use client'
 
-import { ListIcon, useToast } from '@chakra-ui/react'
-
 // Layout
-export * from './components/AspectRatio'
-export * from './components/Box'
-export * from './components/Center'
-export * from './components/Square'
-export * from './components/Circle'
-export * from './components/Container'
-export * from './components/Flex'
-export * from './components/Grid'
-export * from './components/GridItem'
-export * from './components/SimpleGrid'
-export * from './components/Stack'
+export { AspectRatio } from './components/AspectRatio'
+export type { AspectRatioProps } from './components/AspectRatio'
+
+export { Box } from './components/Box'
+export type { BoxProps } from './components/Box'
+
+export { Center } from './components/Center'
+export type { CenterProps } from './components/Center'
+
+export { Square } from './components/Square'
+export type { SquareProps } from './components/Square'
+
+export { Circle } from './components/Circle'
+export type { CircleProps } from './components/Circle'
+
+export { Container } from './components/Container'
+export type { ContainerProps } from './components/Container'
+
+export { Flex } from './components/Flex'
+export type { FlexProps } from './components/Flex'
+
+export { Grid } from './components/Grid'
+export type { GridProps } from './components/Grid'
+
+export { GridItem } from './components/GridItem'
+export type { GridItemProps } from './components/GridItem'
+
+export { SimpleGrid } from './components/SimpleGrid'
+export type { SimpleGridProps } from './components/SimpleGrid'
+
+export { Stack } from './components/Stack'
+export type { StackProps } from './components/Stack'
 export { HStack } from './components/HStack'
 export { VStack } from './components/VStack'
-export * from './components/Spacer'
-export * from './components/Wrap'
+
+export { Spacer } from './components/Spacer'
+export type { SpacerProps } from './components/Spacer'
+
+export { Wrap } from './components/Wrap'
+export type { WrapProps } from './components/Wrap'
 
 // Forms
-export * from './components/Button'
-export * from './components/ButtonGroup'
-export * from './components/Checkbox'
-export * from './components/CheckboxGroup'
-export * from './components/Editable'
-export * from './components/EditableInput'
-export * from './components/EditableTextarea'
-export * from './components/EditablePreview'
-export * from './components/FormControl'
-export * from './components/FormLabel'
-export * from './components/FormErrorMessage'
-export * from './components/FormHelperText'
-export * from './components/IconButton'
-export * from './components/Input'
-export * from './components/NumberInput'
-export * from './components/NumberInputField'
-export * from './components/NumberInputStepper'
-export * from './components/NumberIncrementStepper'
-export * from './components/NumberDecrementStepper'
-export * from './components/PinInput'
-export * from './components/PinInputField'
-export * from './components/Radio'
-export * from './components/RadioGroup'
-export * from './components/RangeSlider'
-export * from './components/RangeSliderTrack'
-export * from './components/RangeSliderFilledTrack'
-export * from './components/RangeSliderThumb'
-export * from './components/Select'
-export * from './components/Slider'
-export * from './components/SliderTrack'
+export { Button } from './components/Button'
+export type { ButtonProps } from './components/Button'
+
+export { ButtonGroup } from './components/ButtonGroup'
+export type { ButtonGroupProps } from './components/ButtonGroup'
+
+export { Checkbox } from './components/Checkbox'
+export type { CheckboxProps } from './components/Checkbox'
+
+export { CheckboxGroup } from './components/CheckboxGroup'
+export type { CheckboxGroupProps } from './components/CheckboxGroup'
+
+export { Editable } from './components/Editable'
+export type { EditableProps } from './components/Editable'
+
+export { EditableInput } from './components/EditableInput'
+export type { EditableInputProps } from './components/EditableInput'
+
+export { EditableTextarea } from './components/EditableTextarea'
+export type { EditableTextareaProps } from './components/EditableTextarea'
+
+export { EditablePreview } from './components/EditablePreview'
+export type { EditablePreviewProps } from './components/EditablePreview'
+
+export { FormControl } from './components/FormControl'
+export type { FormControlProps } from './components/FormControl'
+
+export { FormLabel } from './components/FormLabel'
+export type { FormLabelProps } from './components/FormLabel'
+
+export { FormErrorMessage } from './components/FormErrorMessage'
+export type { FormErrorMessageProps } from './components/FormErrorMessage'
+
+export { FormHelperText } from './components/FormHelperText'
+export type { FormHelperTextProps } from './components/FormHelperText'
+
+export { IconButton } from './components/IconButton'
+export type { IconButtonProps } from './components/IconButton'
+
+export { Input } from './components/Input'
+export type { InputProps } from './components/Input'
+
+export { NumberInput } from './components/NumberInput'
+export type { NumberInputProps } from './components/NumberInput'
+
+export { NumberInputField } from './components/NumberInputField'
+export type { NumberInputFieldProps } from './components/NumberInputField'
+
+export { NumberInputStepper } from './components/NumberInputStepper'
+export type { NumberInputStepperProps } from './components/NumberInputStepper'
+
+export { NumberIncrementStepper } from './components/NumberIncrementStepper'
+export type { NumberIncrementStepperProps } from './components/NumberIncrementStepper'
+
+export { NumberDecrementStepper } from './components/NumberDecrementStepper'
+export type { NumberDecrementStepperProps } from './components/NumberDecrementStepper'
+
+export { PinInput } from './components/PinInput'
+export type { PinInputProps } from './components/PinInput'
+
+export { PinInputField } from './components/PinInputField'
+export type { PinInputFieldProps } from './components/PinInputField'
+
+export { Radio } from './components/Radio'
+export type { RadioProps } from './components/Radio'
+
+export { RadioGroup } from './components/RadioGroup'
+export type { RadioGroupProps } from './components/RadioGroup'
+
+export { RangeSlider } from './components/RangeSlider'
+export type { RangeSliderProps } from './components/RangeSlider'
+
+export { RangeSliderTrack } from './components/RangeSliderTrack'
+export type { RangeSliderTrackProps } from './components/RangeSliderTrack'
+
+export { RangeSliderFilledTrack } from './components/RangeSliderFilledTrack'
+export type { RangeSliderInnerTrackProps } from './components/RangeSliderFilledTrack'
+
+export { RangeSliderThumb } from './components/RangeSliderThumb'
+export type { RangeSliderThumbProps } from './components/RangeSliderThumb'
+
+export { Select } from './components/Select'
+export type { SelectProps } from './components/Select'
+
+export { Slider } from './components/Slider'
+export type { SliderProps } from './components/Slider'
+
+export { SliderTrack } from './components/SliderTrack'
+export type { SliderTrackProps } from './components/SliderTrack'
 export { SliderFilledTrack } from './components/SliderFilledTrack'
-export * from './components/SliderThumb'
-export * from './components/SliderMark'
-export * from './components/Switch'
-export * from './components/Textarea'
+
+export { SliderThumb } from './components/SliderThumb'
+export type { SliderThumbProps } from './components/SliderThumb'
+
+export { SliderMark } from './components/SliderMark'
+export type { SliderMarkProps } from './components/SliderMark'
+
+export { Switch } from './components/Switch'
+export type { SwitchProps } from './components/Switch'
+
+export { Textarea } from './components/Textarea'
+export type { TextareaProps } from './components/Textarea'
 
 // Data Display
-export * from './components/Badge'
-export * from './components/Card'
-export * from './components/CardHeader'
-export * from './components/CardBody'
-export * from './components/CardFooter'
-export * from './components/Code'
-export * from './components/Divider'
-export * from './components/Kbd'
-export * from './components/List'
-export * from './components/ListItem'
-export { ListIcon }
+export { Badge } from './components/Badge'
+export type { BadgeProps } from './components/Badge'
+
+export { Card } from './components/Card'
+export type { CardProps } from './components/Card'
+
+export { CardHeader } from './components/CardHeader'
+export type { CardHeaderProps } from './components/CardHeader'
+
+export { CardBody } from './components/CardBody'
+export type { CardBodyProps } from './components/CardBody'
+
+export { CardFooter } from './components/CardFooter'
+export type { CardFooterProps } from './components/CardFooter'
+
+export { Code } from './components/Code'
+export type { CodeProps } from './components/Code'
+
+export { Divider } from './components/Divider'
+export type { DividerProps } from './components/Divider'
+
+export { Kbd } from './components/Kbd'
+export type { KbdProps } from './components/Kbd'
+
+export { List } from './components/List'
+export type { ListProps } from './components/List'
 export { OrderedList } from './components/OrderedList'
 export { UnorderedList } from './components/UnorderedList'
-export * from './components/Stat'
-export * from './components/StatLabel'
-export * from './components/StatNumber'
-export * from './components/StatHelpText'
-export * from './components/StatArrow'
-export * from './components/StatGroup'
-export * from './components/Table'
-export * from './components/Thead'
-export * from './components/Tbody'
-export * from './components/Tfoot'
-export * from './components/Tr'
-export * from './components/Th'
-export * from './components/Td'
-export * from './components/TableCaption'
-export * from './components/TableContainer'
-export * from './components/Tag'
-export * from './components/TagLabel'
+
+export { ListItem } from './components/ListItem'
+export type { ListItemProps } from './components/ListItem'
+
+export { Stat } from './components/Stat'
+export type { StatProps } from './components/Stat'
+
+export { StatLabel } from './components/StatLabel'
+export type { StatLabelProps } from './components/StatLabel'
+
+export { StatNumber } from './components/StatNumber'
+export type { StatNumberProps } from './components/StatNumber'
+
+export { StatHelpText } from './components/StatHelpText'
+export type { StatHelpTextProps } from './components/StatHelpText'
+
+export { StatArrow } from './components/StatArrow'
+export type { StatArrowProps } from './components/StatArrow'
+
+export { StatGroup } from './components/StatGroup'
+export type { StatGroupProps } from './components/StatGroup'
+
+export { Table } from './components/Table'
+export type { TableProps } from './components/Table'
+
+export { Thead } from './components/Thead'
+export type { TableHeadProps } from './components/Thead'
+
+export { Tbody } from './components/Tbody'
+export type { TableBodyProps } from './components/Tbody'
+
+export { Tfoot } from './components/Tfoot'
+export type { TableFooterProps } from './components/Tfoot'
+
+export { Tr } from './components/Tr'
+export type { TableRowProps } from './components/Tr'
+
+export { Th } from './components/Th'
+export type { TableColumnHeaderProps } from './components/Th'
+
+export { Td } from './components/Td'
+export type { TableCellProps } from './components/Td'
+
+export { TableCaption } from './components/TableCaption'
+export type { TableCaptionProps } from './components/TableCaption'
+
+export { TableContainer } from './components/TableContainer'
+export type { TableContainerProps } from './components/TableContainer'
+
+export { Tag } from './components/Tag'
+export type { TagProps } from './components/Tag'
+
+export { TagLabel } from './components/TagLabel'
+export type { TagLabelProps } from './components/TagLabel'
+
 export { TagLeftIcon } from './components/TagLeftIcon'
+export type { IconProps } from './components/TagLeftIcon'
 export { TagRightIcon } from './components/TagRightIcon'
-export * from './components/TagCloseButton'
+
+export { TagCloseButton } from './components/TagCloseButton'
+export type { TagCloseButtonProps } from './components/TagCloseButton'
 
 // Feedback
-export * from './components/Alert'
-export * from './components/AlertIcon'
-export * from './components/AlertTitle'
-export * from './components/AlertDescription'
-export * from './components/CircularProgress'
-export * from './components/CircularProgressLabel'
-export * from './components/Progress'
-export * from './components/Skeleton'
+export { Alert } from './components/Alert'
+export type { AlertProps } from './components/Alert'
+
+export { AlertIcon } from './components/AlertIcon'
+export type { AlertIconProps } from './components/AlertIcon'
+
+export { AlertTitle } from './components/AlertTitle'
+export type { AlertTitleProps } from './components/AlertTitle'
+
+export { AlertDescription } from './components/AlertDescription'
+export type { AlertDescriptionProps } from './components/AlertDescription'
+
+export { CircularProgress } from './components/CircularProgress'
+export type { CircularProgressProps } from './components/CircularProgress'
+
+export { CircularProgressLabel } from './components/CircularProgressLabel'
+export type { CircularProgressLabelProps } from './components/CircularProgressLabel'
+
+export { Progress } from './components/Progress'
+export type { ProgressProps } from './components/Progress'
+
+export { Skeleton } from './components/Skeleton'
+export type { SkeletonProps } from './components/Skeleton'
 export { SkeletonCircle } from './components/SkeletonCircle'
-export * from './components/SkeletonText'
-export * from './components/Spinner'
-export { useToast }
+
+export { SkeletonText } from './components/SkeletonText'
+export type { SkeletonTextProps } from './components/SkeletonText'
+
+export { Spinner } from './components/Spinner'
+export type { SpinnerProps } from './components/Spinner'
 
 // Typography
 export { Text } from '@chakra-ui/react'
+export { Link } from '@chakra-ui/react'
+
 export { Heading } from '@chakra-ui/react'
+export type { HeadingProps } from '@chakra-ui/react'
+
 export { Highlight } from '@chakra-ui/react'
+export type { HighlightProps } from '@chakra-ui/react'
 
 // Overlay
 export {
@@ -189,7 +358,6 @@ export {
 } from '@chakra-ui/react'
 
 // Media and Icons
-export * from '@chakra-ui/icons'
 export { Avatar, AvatarBadge, AvatarGroup } from '@chakra-ui/react'
 export { Icon, createIcon } from '@chakra-ui/react'
 export { Image } from '@chakra-ui/react'
@@ -211,9 +379,10 @@ export {
 export type { SystemStyleObject } from '@chakra-ui/react'
 
 export { mode } from '@chakra-ui/theme-tools'
-
 export type { ColorMode } from '@chakra-ui/react'
 
 export { useColorMode } from '@chakra-ui/react'
 
 export { extendTheme } from '@chakra-ui/react'
+
+export { useToast, ListIcon } from '@chakra-ui/react'
