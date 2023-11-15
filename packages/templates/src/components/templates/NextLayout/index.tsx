d41ff876f890882b0f61be3df3bdeae0dbccf3d5
Pedro Duarte <pedrohlduarte@gmail.com>
2023-11-15 18:34:18 -0300

    feat: remove templates

--- packages/templates/src/components/templates/NextLayout/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from 'react'
-
-interface NextLayoutProps {
-  title?: string
-}
-
-export function NextLayout({ children }: PropsWithChildren<NextLayoutProps>) {
-  return <main>{children}</main>
-}
