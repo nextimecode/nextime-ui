f63ea6619f5affe1f8143b7e4c01cadaf0bbc800
Pedro Duarte <pedrohlduarte@gmail.com>
2024-04-24 00:22:20 -0300

    feat/update eslint config (#22)
    * feat: update libs and eslint
    
    * build: update libs
    
    * build: update libs

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,20 +1,20 @@
-import { Box, BoxProps } from '@nextime-ui/react'
+// import { Box, BoxProps } from '@nextime-ui/react'
 
-import type { Meta, Story } from '@storybook/react'
+// import type { Meta, Story } from '@storybook/react'
 
-const meta: Meta<BoxProps> = {
-  title: 'LAYOUT/Box',
-  component: Box,
-  tags: ['autodocs'],
-  args: {
-    children: 'This is the Box'
-  },
-  argTypes: {}
-}
+// const meta: Meta<BoxProps> = {
+//   title: 'LAYOUT/Box',
+//   component: Box,
+//   tags: ['autodocs'],
+//   args: {
+//     children: 'This is the Box'
+//   },
+//   argTypes: {}
+// }
 
-const Usage: Story<BoxProps> = args => <Box {...args} />
+// const Usage: Story<BoxProps> = args => <Box {...args} />
 
-Usage.args = {}
+// Usage.args = {}
 
-export default meta
-export { Usage }
+// export default meta
+// export { Usage }
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -8,6 +8,7 @@ module.exports = {
     'standard',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:unicorn/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -20,9 +21,19 @@ module.exports = {
   plugins: [
     'jsx-a11y',
     '@typescript-eslint',
-    "eslint-plugin-import-helpers"
+    "eslint-plugin-import-helpers",
+    'prettier',
+    'unicorn',
+    'perfectionist',
   ],
   rules: {
+    'space-before-blocks': 'error',
+    'keyword-spacing': 'error',
+    'arrow-spacing': 'error',
+    'semi-spacing': 'error',
+    'no-redeclare': 'error',
+    'no-console': 'error',
+    'no-undef': 'error',
     'comma-dangle': 'off',
     "import-helpers/order-imports": [
       "error",
@@ -67,6 +78,22 @@ module.exports = {
     'jsx-a11y/aria-unsupported-elements': 'warn',
     'jsx-a11y/role-has-required-aria-props': 'warn',
     'jsx-a11y/role-supports-aria-props': 'warn',
+    'array-bracket-spacing': ['error', 'never'],
+    'computed-property-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'spaced-comment': ['error', 'always'],
+    'template-tag-spacing': 'error',
+    'prettier/prettier': 'error',
+    'unicorn/prevent-abbreviations': [0, { ignore: ['/.*/ig'] }],
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-object-from-entries': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+    'unicorn/no-thenable': 'off',
+    'unicorn/prefer-top-level-await': 'off',
+    'unicorn/text-encoding-identifier-case': 'off',
+    'unicorn/filename-case': 'off',
+    'sonarjs/no-duplicate-string': 'off'
   },
   settings: {
     react: {
