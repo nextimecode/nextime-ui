7eb967436b8da50f01b3cfa24f8b47fd7c1820e4
Pedro Duarte <pedrohlduarte@gmail.com>
2024-11-23 20:51:23 -0300

    style: clean up vite.config.js by removing unnecessary comments and organizing imports

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -1,8 +1,7 @@
-/// <reference types="vite/client" />
-
-import { defineConfig } from 'vite'
+// vite.config.js
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 })
